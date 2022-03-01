cd3d195a538d4b7c88f416fef31042857c80e0bf
Hammad <muhammadhammadali112233@gmail.com>
2022-03-01 12:11:44 +0500

    Here we Create only Stack navigation and only create the home bare in stack navigation Not apply the stack on different screens

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-const Home = () => {
+
+export default function Home() {
+  const description="Lorem ipsum dolor,  impedit molestias similique dolorum sit tempora nesciunt error et iure officia deserunt ullam dolore, mollitia fugiat. Dolores, consequatur!"
   return (
-    <View>
-      <Text>My First Online App By Hammad</Text>
+    <View style={styles.mainContainer}>
+        <View style={styles.homeTop}>
+            <Image
+            style={styles.imageHeader}
+            resizeMode='contain'
+            source={require('../../assets/student.jpg')}
+            
+            />
+            <Text style={styles.header}>Welcome To </Text>
+            <Text style={[styles.header,{
+              fontSize:20,
+              color:'blue',
+              marginTop:0,
+            }]}>Hammad Channel</Text>
+
+            <Text style={styles.para}>{description}</Text>
+
+        </View>
     </View>
-  )
+  );
 }
 
-export default Home
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    height:'100%',
+    justifyContent:'space-between',   //yeah by default colum wise hota hai lakin aisa karnay say yeah row wise behave karay ga
+    paddingHorizontal:20,
+    textAlign:'center',
+  },  
+  imageHeader:{
+    height:undefined,
+    width:"100%",
+    aspectRatio:1,
+    marginTop:50,
+    borderRadius:10,
+  },
+  homeTop:{
+    display:"flex",
+    justifyContent:'center',
+    alignItems:'center',
+    paddingHorizontal:10,
+  },
+  header:{
+    textTransform:'uppercase',
+    fontSize:30,
+    color:'#344055',
+  },
+  para:{
+      marginTop:30,
+      lineHeight:26,
+      paddingBottom:50,
+      textAlign:'left',
+  },
+})
